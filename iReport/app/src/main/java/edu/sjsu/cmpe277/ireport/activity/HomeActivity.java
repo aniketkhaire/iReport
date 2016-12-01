9a4eeaace75b25ac9cd53f03dbe0b4ed54aa59e9
Aniket Khaire <aniket.khaire@ooma.com>
2016-12-01 14:24:25 -0800

    Initail Commit. Basic folder layout

---
+++ iReport/app/src/main/java/edu/sjsu/cmpe277/ireport/activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package edu.sjsu.cmpe277.ireport.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.sjsu.cmpe277.ireport.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
